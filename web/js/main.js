0d3a513d9217d27e9520b87e0d0f704b174f6c88
Cuong <thcwefh@gmail.com>
2018-01-24 18:21:38 -0800

    add fairy and knight

--- web/js/main.js
+++ web/js/main.js
@@ -1,8 +1,19 @@
 var AM = new AssetManager();
 
 AM.queueDownload("./img/elf/1/1_IDLE.png");
-//AM.queueDownload("./img/Elf1.png");
-//AM.queueDownload("./img/background.jpg");
+
+AM.queueDownload("./img/fairy/1/2_WALK.png");
+AM.queueDownload("./img/fairy/1/6_ATTACK.png");
+AM.queueDownload("./img/fairy/1/7_HURT.png");
+AM.queueDownload("./img/fairy/1/1_IDLE.png");
+AM.queueDownload("./img/fairy/1/8_DIE.png");
+
+AM.queueDownload("./img/knight/2_KNIGHT/WALK.png");
+AM.queueDownload("./img/knight/2_KNIGHT/ATTACK.png");
+AM.queueDownload("./img/knight/2_KNIGHT/HURT.png");
+AM.queueDownload("./img/knight/2_KNIGHT/IDLE.png");
+AM.queueDownload("./img/knight/2_KNIGHT/DIE.png");
+
 
 AM.downloadAll(function () {
     var canvas = document.getElementById("gameWorld");
@@ -13,10 +24,19 @@ AM.downloadAll(function () {
     gameEngine.start();
     
 //    gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/background.jpg"), 1024, 512));
-    gameEngine.addEntity(new Elf(
-    		gameEngine, 
-    		AM.getAsset("./img/elf/1/1_IDLE.png"))
-    );
+    gameEngine.addEntity(new Elf(gameEngine, AM.getAsset("./img/elf/1/1_IDLE.png")));
+    
+    
+    
+    
+    gameEngine.addEntity(new Fairy(gameEngine, AM.getAsset("./img/fairy/1/2_WALK.png")));
+//    gameEngine.addEntity(new Fairy(gameEngine, AM.getAsset("./img/fairy/1/6_ATTACK.png")));
+//    gameEngine.addEntity(new Fairy(gameEngine, AM.getAsset("./img/fairy/1/7_HURT.png")));
+//    gameEngine.addEntity(new Fairy(gameEngine, AM.getAsset("./img/fairy/1/1_IDLE.png")));
+//    gameEngine.addEntity(new Fairy(gameEngine, AM.getAsset("./img/fairy/1/8_DIE.png")));
 
+    gameEngine.addEntity(new Knight(gameEngine, AM.getAsset("./img/knight/2_KNIGHT/WALK.png")));
     console.log("All Done!");
+    
+    
 });
