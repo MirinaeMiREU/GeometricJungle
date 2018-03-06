28c840ce5434d6e2ccb528490d573f9749056aae
varikmp <varikmp@varikmp-gazelle>
2018-03-05 17:50:19 -0800

    finished the arrow flying from the elf while attacking

--- js/constant.js
+++ js/constant.js
@@ -10,11 +10,13 @@ const ELF_LEFT_IDLE = "elf.left.idle";
 const ELF_LEFT_WALK = "elf.left.walk";
 const ELF_LEFT_ATTACK = "elf.left.attack";
 const ELF_LEFT_DIE = "elf.left.die";
+const ELF_LEFT_ARROW = "elf.left.arrow";
 
 const ELF_RIGHT_IDLE = "elf.right.idle";
 const ELF_RIGHT_WALK = "elf.right.walk";
 const ELF_RIGHT_ATTACK = "elf.right.attack";
 const ELF_RIGHT_DIE = "elf.right.die";
+const ELF_RIGHT_ARROW = "elf.right.arrow";
 
 const KNIGHT_LEFT_IDLE = "knight.left.idle";
 const KNIGHT_LEFT_WALK = "knight.left.walk";
